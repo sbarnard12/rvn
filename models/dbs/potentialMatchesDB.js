ae2b52234b5bd1b0997b02dc9bf7cf9311ead1ec
Sam Barnard <sambarnard12@gmail.com>
2017-03-10 01:13:24 -0500

    typos

---
+++ models/dbs/potentialMatchesDB.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose'),
+    db;
+
+db = mongoose.createConnection('mongodb://db_admin:DBAdmin1@ds127300.mlab.com:27300/potentialmatches');
+
+db.on('error', function(err){
+    if(err) throw err;
+});
+
+db.once('open', function callback () {
+    console.info('Mongo db connected successfully');
+});
+
+
+module.exports = db;
