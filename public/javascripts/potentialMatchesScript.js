d57a4fb5542e56b7d01703eebb9922d0960e2c80
danyal <danyalmont@yahoo.ca>
2017-03-13 17:23:45 -0400

    Signup Page updated

--- public/javascripts/potentialMatchesScript.js
+++ public/javascripts/potentialMatchesScript.js
@@ -33,6 +33,82 @@ var setMatchedButton = function(){
         })
     })
 }
+var General_Information = document.getElementById('General_Information');
+var Work_and_Education = document.getElementById('Work_and_Education');
+var Contact_Information = document.getElementById('Contact_Information');
+var Interests = document.getElementById('Interests');
+
+$(function page_start() {
+        General_Information.setAttribute('class', 'visible');
+        Work_and_Education.setAttribute('class', 'hidden');
+        Contact_Information.setAttribute('class', 'hidden');
+        Interests.setAttribute('class', 'hidden');
+})
+
+function displayGeneralInformation() {
+        General_Information.setAttribute('class', 'visible');
+        Work_and_Education.setAttribute('class', 'hidden');
+        Contact_Information.setAttribute('class', 'hidden');
+        Interests.setAttribute('class', 'hidden');
+
+}
+
+function displayWorkEducation() {
+        General_Information.setAttribute('class', 'hidden');
+        Work_and_Education.setAttribute('class', 'visible');
+        Contact_Information.setAttribute('class', 'hidden');
+        Interests.setAttribute('class', 'hidden');
+}
+
+function displayContactInformation() {
+        General_Information.setAttribute('class', 'hidden');
+        Work_and_Education.setAttribute('class', 'hidden');
+        Contact_Information.setAttribute('class', 'visible');
+        Interests.setAttribute('class', 'hidden');
+}
+
+function displayInterests() {
+        General_Information.setAttribute('class', 'hidden');
+        Work_and_Education.setAttribute('class', 'hidden');
+        Contact_Information.setAttribute('class', 'hidden');
+        Interests.setAttribute('class', 'visible');
+}
+
+$(function(){
+    $('#reviews_nav').on('click', getPartial);
+    $('#taskHistory_nav').on('click', getPartial);
+    $('#currentTasks_nav').on('click', getPartial);
+    $('#profile_nav').on('click', getPartial);
+    $('td:first-child').each(taskDetailLink);
+    $('.complete_button').each(completeButton);
+})
+
+var getPartial = function(){
+    console.log("test");
+    var tabString = this.id.split("_")[0];
+    var id = this.name;
+    var url = 'profile/' + id;
+    var redirect = "http://localhost:3000/user/" + tabString + "/" + id;
+
+    window.location = (redirect);
+}
+
+var taskDetailLink = function(){
+    $(this).parent().on('click', function(){
+        var id = this.firstElementChild.innerText; 
+        var redirect = "http://localhost:3000/taskDetails/" + id;
+        window.location = (redirect);
+    })
+}
+
+var completeButton = function(){
+    $(this).on('click', function(){
+        event.stopPropagation();
+        var id = this.id;
+        var url = "http://localhost:3000/taskreview/" + id;
+        window.location = (url);
+    })
+}
 
 
 
