3100c39eaa5b04e15a19b8407084989a9000906c
danyal <danyalmont@yahoo.ca>
2017-03-13 22:38:07 -0400

    WIP interested users

--- public/javascripts/potentialMatchesScript.js
+++ public/javascripts/potentialMatchesScript.js
@@ -33,6 +33,42 @@ var setMatchedButton = function(){
         })
     })
 }
+
+/*$(function(){
+    $('td:first-child').each(userProfileLink);
+    $('td:last-child').each(setMatchedButton)
+})
+
+var userProfileLink = function(){
+    $(this).parent().on('click', function(){
+        var id = this.firstElementChild.innerText;
+        var redirect = "http://localhost:3000/user/profile/" + id;
+        window.location = (redirect);
+    })
+}
+
+var setMatchedButton = function(){
+    //not working under tr onclick
+    $(this).children().on('click', function(ev){
+        ev.stopPropagation();
+        var user_id = this.closest('tr').firstElementChild.innerText
+        var split = window.location.href.split("/");
+        var task_id = split[4];
+
+         $.ajax({
+            url: 'http://localhost:3000/interestedUsers',
+            type: 'POST',
+            data: {task_id: task_id, user_id: user_id},
+            success: function(result){
+                if(result === "success"){
+                    console.log("success");
+                } else {
+                    console.log("Fail");
+                }
+            }
+        })
+    })
+}
 var General_Information = document.getElementById('General_Information');
 var Work_and_Education = document.getElementById('Work_and_Education');
 var Contact_Information = document.getElementById('Contact_Information');
@@ -119,3 +155,4 @@ var completeButton = function(){
 
 
 
+*/
