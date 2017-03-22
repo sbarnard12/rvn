7537996b1eecfdc6fb2d9719897015107177fc32
Sam Barnard <sambarnard12@gmail.com>
2017-03-22 02:07:56 -0400

    changed banner

--- public/javascripts/searchScript.js
+++ public/javascripts/searchScript.js
@@ -6,6 +6,10 @@ $(function(){
             search();
         }
     });
+    $('#reviews_nav').on('click', getPartial);
+    $('#taskHistory_nav').on('click', getPartial);
+    $('#currentTasks_nav').on('click', getPartial);
+    $('#profile_nav').on('click', getPartial);
 })
 
 
@@ -18,4 +22,14 @@ var search = function(){
 var newTask = function(){
 	var redirect = "http://localhost:3000/createtask"
 	window.location = (redirect);
-}
+};
+
+var getPartial = function(){
+    console.log("test");
+    var tabString = this.id.split("_")[0];
+    var id = this.name;
+    var url = 'profile/' + id;
+    var redirect = "http://localhost:3000/user/" + tabString + "/" + id;
+
+    window.location = (redirect);
+};
